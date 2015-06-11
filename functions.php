bd1a2edcd27a7cb931c434eed2ea8643b148690e
Francois Botha <igitur@gmail.com>
2015-06-12 01:24:21 +0200

    Add woocommerce support

--- functions.php
+++ functions.php
@@ -53,6 +53,7 @@ function sparkling_setup() {
 
   // Add default posts and comments RSS feed links to head.
   add_theme_support( 'automatic-feed-links' );
+  add_theme_support('woocommerce');
 
   /**
    * Enable support for Post Thumbnails on posts and pages.
